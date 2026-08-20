d342cc7bd26b50dfc50720ecbb4b5997e31e1df3
Breno <breno.fsouza6@gmail.com>
2026-08-20 14:34:05 -0300

    aula2

--- front/criar.php
+++ front/criar.php
@@ -4,13 +4,10 @@
 <head>
 
     <meta charset="UTF-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Criar Materiais</title>
 
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-
     <link rel="stylesheet" href="../css/navbar.css">
 
 </head>
@@ -33,26 +30,15 @@
               enctype="multipart/form-data">
 
 
-            <!-- TÍTULO -->
-
             <div class="campo">
 
                 <label for="titulo">
                     Título do material
                 </label>
 
-                <input
-                    type="text"
-                    id="titulo"
-                    name="titulo"
-                    placeholder="Digite o título do material"
-                    required
-                >
+                <input type="text" id="titulo" name="titulo" placeholder="Digite o título do material" required>
 
             </div>
-
-
-            <!-- DISCIPLINA -->
 
             <div class="campo">
 
@@ -63,18 +49,10 @@
                 <div class="campo-busca">
 
                     <input
-                        type="text"
-                        id="disciplina"
-                        name="disciplina"
-                        placeholder="Digite para pesquisar..."
-                        autocomplete="off"
-                        required
-                    >
+                        type="text" id="disciplina" name="disciplina" placeholder="Digite para pesquisar..." autocomplete="off" required>
 
                     <input
-                        type="hidden"
-                        id="id_disciplina"
-                        name="id_disciplina"
+                        type="hidden" id="id_disciplina"  name="id_disciplina"
                     >
 
                     <div
@@ -94,9 +72,6 @@
                 </button>
 
             </div>
-
-
-            <!-- CONTEÚDO -->
 
             <div class="campo">
 
@@ -140,8 +115,6 @@
             </div>
 
 
-            <!-- NÍVEL DE ENSINO -->
-
             <div class="campo">
 
                 <label for="nivel">
@@ -178,8 +151,6 @@
             </div>
 
 
-            <!-- ARQUIVO -->
-
             <div class="campo">
 
                 <label for="arquivo">
@@ -207,12 +178,6 @@
         </form>
 
     </main>
-
-
-
-    <!-- ============================= -->
-    <!-- MODAL DISCIPLINA -->
-    <!-- ============================= -->
 
     <dialog
         class="modal"
@@ -263,11 +228,6 @@
     </dialog>
 
 
-
-    <!-- ============================= -->
-    <!-- MODAL CONTEÚDO -->
-    <!-- ============================= -->
-
     <dialog
         class="modal"
         id="modalConteudo">
@@ -316,100 +276,44 @@
 
     </dialog>
 
-
-
-    <!-- ============================= -->
-    <!-- NAVBAR -->
-    <!-- ============================= -->
-
     <nav class="sidebar-navigation">
-
         <ul>
-
             <li>
-
                 <a href="../index.php">
-
                     <i class="bx bx-home-alt"></i>
-
-                    <span class="tooltip">
-                        Inicio
-                    </span>
-
+                    <span class="tooltip">Inicio</span>
                 </a>
-
             </li>
-
-
-            <li class="active">
-
+            <li>
                 <a href="criar.php">
-
                     <i class="bx bx-plus"></i>
-
-                    <span class="tooltip">
-                        Criar
-                    </span>
-
+                    <span class="tooltip">Criar</span>
                 </a>
-
             </li>
-
-
             <li>
-
                 <a href="/pesquisar.php">
-
                     <i class="bx bx-search-alt"></i>
-
-                    <span class="tooltip">
-                        Pesquisar
-                    </span>
-
+                    <span class="tooltip">Pesquisar</span>
                 </a>
-
             </li>
-
-
             <li>
-
                 <a href="planejamento.php">
-
                     <i class="bx bx-calendar-event"></i>
-
-                    <span class="tooltip">
-                        Planejamento
-                    </span>
-
+                    <span class="tooltip">Planejamento</span>
                 </a>
-
             </li>
-
-
-            <li>
-
+            <li class="active">
                 <a href="perfil.php">
-
                     <i class="bx bx-user"></i>
-
-                    <span class="tooltip">
-                        Perfil
-                    </span>
-
+                    <span class="tooltip">Perfil</span>
                 </a>
-
             </li>
-
         </ul>
-
     </nav>
 
 
-
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-
     <script src="../js/navbar.js"></script>
-
     <script src="../js/criar.js"></script>
 
 </body>
--- index.php
+++ index.php
@@ -1,89 +1,100 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
-<html lang="PT-BR">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Página Inicial</title>
+<html lang="en">
 
- <link rel="stylesheet" href="css/navbar.css">
- <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Página Inicial</title>
+
+	<link rel="stylesheet" href="css/inicio.css">
+	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
 </head>
+
 <body>
 
-	<div class="logo">
-        <img src="img/logo.png">
-    </div>	
-
-  <nav class="sidebar-navigation">
-
-  <ul>
-		<li class="active">
-			<a href="index.php">
-				<i class="bx bx-home-alt"></i>
-				<span class="tooltip">Inicio</span>
-			</a>
-		</li>
-
-		<li>
-			<a href="front/criar.php">
-				<i class="bx bx-plus"></i>
-				<span class="tooltip">Criar</span>
-			</a>
-		</li>
-
-		<li>
-			<a href="front/pesquisar.php">
-				<i class="bx bx-search-alt"></i>
-				<span class="tooltip">Pesquisar</span>
-			</a>
-		</li>
-
-		<li>
-			<a href="front/planejamento.php">
-				<i class="bx bx-calendar-event"></i>
-				<span class="tooltip">Planejamento</span>
-			</a>
-		</li>
-
-		<li>
-			<a href="front/perfil.php">
-				<i class="bx bx-user"></i>
-				<span class="tooltip">Perfil</span>
-			</a>
-		</li>
-	</ul>
-</nav>
-
-	<form action="/pesquisa" method="get">
-  		<input type="search" name="q" placeholder="Buscar usuários, conteúdos ou materiais..." required>
-  			<button type="submit">Buscar</button>
-		</form>
-
-		<div>
-			<h3>Explore novas possibilidades para suas aulas.</h3>
-			<h1>Organize seu conhecimento de forma inteligente.</h1>
-			<h3>Crie, organize e compartilhe questões e materiais em um só lugar.
-			Sirius conecta você a conteúdos de outros professores.</h3>
+	<div class="topbar">
+		<div class="logo">
+			<img src="img/logo.png" alt="Logo">
 		</div>
 
-<?php
-if(empty($_SESSION['nome'])){
-	$foto = 'img/user.webp';	
-	echo "<a href='front/perfil.php'><img src='$foto' class='fotoPerfil'></a>";
-	echo "<a href='front/logar.php'>Entrar</a>";
-	
-}else{
-	if (empty($_SESSION['foto_usuario'])) { 
-		$foto = 'img/user.webp'; 
-	} else { 
-		$foto = $_SESSION['foto_usuario']; 
-	}
-	echo "<p>Olá, " . $_SESSION['nome'] . "!</p><a href='front/perfil.php'>'<img src='$foto' class='fotoPerfil'></a>";
-}
-?>
+		<form class="search-bar" action="/pesquisa" method="get">
+			<input type="search" name="q" placeholder="Buscar usuários, conteúdos ou materiais..." required>
+			<button type="submit">Buscar</button>
+		</form>
 
-<script src="js/navbar.js"></script>
+		<div class="user">
+			<?php
+			if (empty($_SESSION['nome'])) {
+				$foto = 'img/user.webp';
+				echo "<a href='front/perfil.php'><img src='$foto' class='fotoPerfil'></a>";
+				echo "<a href='front/logar.php'>Entrar</a>";
+			} else {
+				if (empty($_SESSION['foto_usuario'])) {
+					$foto = 'img/user.webp';
+				} else {
+					$foto = $_SESSION['foto_usuario'];
+				}
+				echo "<p>Olá, " . $_SESSION['nome'] . "!</p><a href='front/perfil.php'><img src='$foto' class='fotoPerfil'></a>";
+			}
+			?>
+		</div>
+	</div>
+
+
+	<nav class="sidebar-navigation">
+
+		<ul>
+			<li class="active">
+				<a href="index.php">
+					<i class="bx bx-home-alt"></i>
+					<span class="tooltip">Inicio</span>
+				</a>
+			</li>
+
+			<li>
+				<a href="front/criar.php">
+					<i class="bx bx-plus"></i>
+					<span class="tooltip">Criar</span>
+				</a>
+			</li>
+
+			<li>
+				<a href="front/pesquisar.php">
+					<i class="bx bx-search-alt"></i>
+					<span class="tooltip">Pesquisar</span>
+				</a>
+			</li>
+
+			<li>
+				<a href="front/planejamento.php">
+					<i class="bx bx-calendar-event"></i>
+					<span class="tooltip">Planejamento</span>
+				</a>
+			</li>
+
+			<li>
+				<a href="front/perfil.php">
+					<i class="bx bx-user"></i>
+					<span class="tooltip">Perfil</span>
+				</a>
+			</li>
+		</ul>
+	</nav>
+
+
+
+	<div>
+		<h3>Explore novas possibilidades para suas aulas.</h3>
+		<h1>Organize seu conhecimento de forma inteligente.</h1>
+		<h3>Crie, organize e compartilhe questões e materiais em um só lugar.
+			Sirius conecta você a conteúdos de outros professores.</h3>
+	</div>
+
+
+
+	<script src="js/navbar.js"></script>
 </body>
+
 </html>
